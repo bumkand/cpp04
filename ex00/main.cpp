d28b373c29a677368b8360d6d604654d6125bab1
bumkand <jakub.andrasi@gmail.com>
2026-02-28 00:58:46 +0100

    ex00 main updated

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -7,10 +7,21 @@ int main()
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+	const Dog s("Dunco");
+	const Cat d("Micka");
+	Animal q = Dog("Zara");
 	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
 	j->makeSound();
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound();
+	std::cout << s.getType() << " " << std::endl;
+	s.makeSound();
+	std::cout << d.getType() << " " << std::endl;
+	d.makeSound();
+	std::cout << q.getType() << " " << std::endl;
+	q.makeSound();
+
+
 	meta->makeSound();
 
 	delete meta;
